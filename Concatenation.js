89d1583e3148bfd2dcb86413f473841a411680a5
Prashant1503 <44309149+Prashant1503@users.noreply.github.com>
2018-12-03 10:19:22 +0530

    Create Concatenation.js

---
+++ Concatenation.js
@@ -0,0 +1,7 @@
+var a1 = 34;
+var a2 = 34;
+document.write(a1+a2,"<br>");
+document.write(a1,a2);
+console.log("This is prashant");
+var age = prompt("Tell me your age");
+document.write("Your age is ",age);

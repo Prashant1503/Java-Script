339e4184be9a822a79211eb4f64fa6a63102aa6c
Prashant1503 <44309149+Prashant1503@users.noreply.github.com>
2018-12-03 10:28:37 +0530

    It will ask to the user

---
+++ Foodcost App.js
@@ -0,0 +1,13 @@
+/*var a1 = 34;
+var a2 = 34;
+document.write(a1+a2,"<br>");
+document.write(a1,a2);
+console.log("This is prashant");
+var age = prompt("Tell me your age");
+document.write("Your age is ",age);
+*/
+
+var foodcost = prompt("Enter the total amout of foodcost");
+var no =prompt("Enter Number of people");
+document.write("Total cost of foodcost was ",foodcost, "<br>");
+document.write("Each one have to pay ", (foodcost/no).toFixed(2), "<br>");

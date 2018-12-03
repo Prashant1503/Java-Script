34e4a7111d0dba5592a0ed27b92ca18a207dff19
Prashant1503 <44309149+Prashant1503@users.noreply.github.com>
2018-12-03 10:18:24 +0530

    creating variable

---
+++ var.js
@@ -0,0 +1,6 @@
+var a1 = 34;
+var a2 = 34;
+document.write(a1+a2,"<br>"); 
+console.log("This is prashant");
+var age = prompt("Tell me your age"); //It will ask to user to enter the age
+document.write("Your age is ",age); //Prints the output
